7ca9e9425618522a18f20e194603f1a6707dbce7
Alejandro Morales <ale95mogra@gmail.com>
2019-03-01 20:50:03 +0100

    En proceso: segunda opcion realizada

--- programaXML.py
+++ programaXML.py
@@ -1,7 +1,24 @@
 from lxml import etree
 doc = etree.parse('parques.xml')
 
+#------------------------ Definicion de variable ----------------------------
+
 lista_municipios = []
+lista_parques = []
+dic_par = {}
+
+#----------------------------- Municipios -----------------------------------
+
+lista_mun = doc.xpath("/result/elements/item/grup_adreca/municipi_nom/text()")   #Lista de todos los municipios incluidas las lineas con \n que no son municipios
+
+for municipio in lista_mun:
+            
+    if municipio.startswith("\n") == False:   #quitamos las lineas con \n 
+                
+        lista_municipios.append(municipio)
+
+#----------------------------------------------------------------------------
+
 
 print("")
 print("-------------------MENU--------------------")
@@ -21,18 +38,48 @@ while condicion != 0:
 
     if condicion == 1:    #Opcion 1: mostrar municipios
 
-        lista_mun = doc.xpath("/result/elements/item/grup_adreca/municipi_nom/text()")   #Lista de todos los municipios incluidas las lineas con \n que no son municipios
-
-        for municipio in lista_mun:
-            
-            if municipio.startswith("\n") == False:   #quitamos las lineas con \n 
-                
-                lista_municipios.append(municipio)
-
         for municipio in sorted(set(lista_municipios)):   #quitamos los municipios duplicados y lo ordenamos
 
             print(municipio)
 
+    if condicion == 2:     #Opcion 2: Contar los parques por Municipios
+
+        contar_municipios = input("Introduce Municipio: ")
+
+        while contar_municipios not in lista_municipios:
+
+            print("")
+            print("--------------------------")
+            print("ERROR, no existe Municipio")
+            print("--------------------------")
+            print("")
+
+            contar_municipios = input("Introduce Municipio: ")
+        
+        print("")
+        print("---------------------------")
+        print("Municipio:",contar_municipios)
+        print("Nº de parques:",lista_municipios.count(contar_municipios))
+        print("---------------------------")
+        print("")
+
+
+
+
+
+            #lista_par = doc.xpath("/result/elements/item/adreca_nom/text()")
+            #
+            #for parque in lista_par:
+            #
+            #    if parque.startswith("\n") == False:   #quitamos las lineas con \n 
+            #    
+            #        lista_parques.append(parque)
+            #
+            #for municipio in  
+            
+            
+
+           # dic_par[doc.xpath("/result/elements/item/grup_adreca/municipi_nom/text()")]=
 
     print("")
     print("-------------------MENU--------------------")
