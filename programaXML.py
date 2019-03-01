bfc6aa8df997afdb0944c7bc481aff2afdad1702
Alejandro Morales <ale95mogra@gmail.com>
2019-03-01 19:43:05 +0100

    En proceso: primera opcion realizada

--- programaXML.py
+++ programaXML.py
@@ -1,6 +1,8 @@
 from lxml import etree
 doc = etree.parse('parques.xml')
 
+lista_municipios = []
+
 print("")
 print("-------------------MENU--------------------")
 print("(1) Mostrar todos los Municipios")
@@ -13,12 +15,24 @@ print("-------------------------------------------")
 print("")
 
 condicion = int(input("¿Que opcion eliges?   "))
+print("")
 
 while condicion != 0:
 
-    if condicion == 1:
+    if condicion == 1:    #Opcion 1: mostrar municipios
 
-        print("")
+        lista_mun = doc.xpath("/result/elements/item/grup_adreca/municipi_nom/text()")   #Lista de todos los municipios incluidas las lineas con \n que no son municipios
+
+        for municipio in lista_mun:
+            
+            if municipio.startswith("\n") == False:   #quitamos las lineas con \n 
+                
+                lista_municipios.append(municipio)
+
+        for municipio in sorted(set(lista_municipios)):   #quitamos los municipios duplicados y lo ordenamos
+
+            print(municipio)
+
 
     print("")
     print("-------------------MENU--------------------")
@@ -32,5 +46,6 @@ while condicion != 0:
     print("")
     
     condicion = int(input("¿Que opcion eliges?   "))
-    
+    print("")
+
 #print(doc.xpath())
